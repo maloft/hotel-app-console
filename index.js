1eadef77f2599cb4395ea61e17aedd4520815b2f
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-24 11:44:40 +0200

    Affichage du menu principal (début)

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log('** Administration Hotel **');
+
+var presentation = require('./presentation.js');
+var start = presentation.start();
---
+++ presentation.js
@@ -0,0 +1,6 @@
+function start(){
+    console.log("1. Lister les clients");
+    console.log("99. Sortir");
+}
+
+exports.start = start;

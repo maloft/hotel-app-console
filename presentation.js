271b8c569cff6d708438995f7a53ea2de9ccff6a
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-24 14:16:09 +0200

    Affichage du menu principale (fin)

--- presentation.js
+++ presentation.js
@@ -1,6 +1,30 @@
+// récupération du module `readline`
+var readline = require('readline');
+
+// création d'un objet `rl` permettant de récupérer la saisie utilisateur
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 function start(){
     console.log("1. Lister les clients");
     console.log("99. Sortir");
+
+    // récupération de la saisie utilisateur
+    rl.question('Votre choix ? ', function(saisie) {
+        if (saisie == 1){
+            console.log(">> Liste des clients");
+        }else if (saisie == 99) {
+            console.log("Au revoir ! ");
+        }else {
+            console.log("Vous avez saisi un mauvais choix");
+        }
+        rl.close();// attention, une fois l'interface fermée, la saisie n'est plus possible
+
+    });
+
+
 }
 
 exports.start = start;
